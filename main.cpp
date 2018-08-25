2b058bf17a1d5d734dfecf3e981b7ecf9bb95ab1
SantiagoPL99 <42584008+SantiagoPL99@users.noreply.github.com>
2018-08-24 19:49:48 -0500

    main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,16 @@ int main()
     List<int>* lista = new List<int>;
     lista-> push_back(3);
     lista-> push_back(1);
+    lista -> push_back(14);
     lista-> push_front(4);
-    cout << lista-> front() << endl;
-    cout << lista-> back() << endl;
+    lista-> print();
+    lista-> pop_back();
+    cout << endl;
+    lista-> print();
+    lista -> push_back(14);
+    cout << endl;
+    lista-> print();
+    lista-> pop_front();
+    cout << endl;
+    lista-> print();
 }
